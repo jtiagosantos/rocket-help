22082164be8121f6645f740814fccb944d4982a4
jtiagosantos <tiagosan040@gmail.com>
2022-07-19 22:41:21 -0400

    feat: 🎸 handle routes by user state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
+//routes
 import { AppRoutes } from './app.routes';
 
+//screens
+import { SignIn } from '../screens/SignIn';
+
+//components
+import { Loading } from '../components/Loading';
+
 export const Routes = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscription = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setIsLoading(false);
+    });
+
+    return subscription;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 };
